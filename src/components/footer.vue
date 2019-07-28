440e538cb0839f633cf6645c4faa43e32e8323da
xinyuanliu <1227653652@qq.com>
2019-07-28 16:29:35 +0800

    'ls'

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,40 +1,43 @@
 <template>
-    <footer>
-        <p>© 2019 Jiang OpenSource:<a href="https://github.com/quietdw/vue-blog">GitHub</a>
-            <a href="https://github.com/quietdw/vue-blog"><svg class="icon" aria-hidden="true">
-    <use xlink:href="#icon-github"></use>
-</svg></a>
-        </p>
-    </footer>
+  <footer>
+    <p>
+      © 2019 lxy:
+      <a href="#">GitHub</a>
+      <a href="https://github.com/LxyoryxL">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-github" />
+        </svg>
+      </a>
+    </p>
+  </footer>
 </template>
 
 <style lang="less" scoped>
 @import url(../assets/base.less);
 
 footer {
-    padding: 10px;
+  padding: 10px;
   align-self: end;
   background-color: #fafbfc;
-  color:#666;
+  color: #666;
   font-size: 13px;
-  padding:  24px 16px;
+  padding: 24px 16px;
   text-align: left;
   p {
-      a{
-              svg{
-          width: 2em;
-          height: 2em;
-          fill: #959da5;
+    a {
+      svg {
+        width: 2em;
+        height: 2em;
+        fill: #959da5;
       }
-      }
-      a:hover{
-          color: #0366d6;
-          text-decoration: underline;
-      }
-      a:nth-child(2){
-          float: right;
-      }
-  
+    }
+    a:hover {
+      color: #0366d6;
+      text-decoration: underline;
+    }
+    a:nth-child(2) {
+      float: right;
+    }
   }
 }
 </style>
